eb5b2a1ab7cf65303ad90b22281406d5caa03e74
yifanl27 <lihello54321@gmail.com>
2021-10-11 22:39:52 -0400

    Created and coded for UserAnalyzer

---
+++ src/main/java/UserAnalyzer.java
@@ -0,0 +1,20 @@
+/*
+This file contains UserAnalyzer class, which is the parent class for all analyzers
+ */
+package main.java;
+
+public class UserAnalyzer implements UserInformation{
+
+    /*
+    Below is the method that all analyzers share, the analyze method.
+    This method takes in any input of UserInformation, and stores it to the variable userInfo.
+    It then returns the type of output that will be specified in the other analyzers.
+    The exact contents will vary then.
+     */
+
+    public Object analyze(UserInformation userInfo){
+        return "";
+    }
+
+    //TODO: More methods may be required in the future?
+}
