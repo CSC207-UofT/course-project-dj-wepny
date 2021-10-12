35bfcc7227672f8c1999e6bb77705a8442c9843a
DAVIDMA0808 <jianxiang.ma@mail.utoronto.ca>
2021-10-12 10:36:05 -0400

    Added some while loops to check for invalid input in the helper methods of Console. Created an instance variable, a constructor, and a new method for RunCommand.

--- src/main/java/Console.java
+++ src/main/java/Console.java
@@ -1,14 +1,14 @@
 package main.java;
 
 import java.util.Scanner;
-// import main.java.Controller
 
 public class Console {
 
-    /**
-     * This class interact with the users and receives their input, then it sends
-     * the inputs to the controller class.
+    /*
+      This class interact with the users and receives their input, then it sends
+      the inputs to the controller class.
      */
+
     // Maybe implement an interface? If so, what would be the name of it?
     // What would it do?
 
@@ -22,17 +22,28 @@ public class Console {
     private static String[] getBasicUserInfo(Scanner reader){
         System.out.println("Please enter your name:");
         String name = reader.nextLine();
+
         System.out.println("Please enter your age:");
         String age = reader.nextLine();
+        while (Integer.parseInt(age) < 0){
+            System.out.println("Invalid input, please try again.");
+            System.out.println("Please enter your age:");
+            age = reader.nextLine();
+        }
+
         System.out.println("Please enter your gender (M/F):");
         String gender = reader.nextLine();
+        while (!gender.equals("M") && !gender.equals("F")){
+            System.out.println("Invalid input, please try again.");
+            System.out.println("Please enter your gender (M/F):");
+            gender = reader.nextLine();
+        }
 
         // Note: later on we would have to make an error checker for the
         // validity of the inputs.
         // e.g age >= 0, Gender must follow the type of M or F, etc.
 
-        String[] result = {name, age, gender};
-        return result;
+        return new String[]{name, age, gender};
     }
 
     /**
@@ -47,43 +58,56 @@ public class Console {
     private static String[] getPersonalUserInfo (Scanner reader){
         System.out.println("Please enter your height (in cm):");
         String height = reader.nextLine();
+        while(Float.parseFloat(height) < 0){
+            System.out.println("Invalid input, please try again.");
+            System.out.println("Please enter your height (in cm):");
+            height = reader.nextLine();
+        }
+
         System.out.println("Please enter your weight (in kg):");
         String weight = reader.nextLine();
-
-        String[] result = {height, weight};
-        return result;
-    }
-
-    /**
-     * A helper method that calls the controller to do operations based on command given.
-     * @param command An integer that represents the option that the user wants to choose.
-     */
-    private static void operations(int command){
-        if (command == 1){
-            // We do something, most likely call the controller to let it call
-            // The BMI analyzer.
+        while(Float.parseFloat(weight) < 0){
+            System.out.println("Invalid input, please try again.");
+            System.out.println("Please enter your weight (in cm):");
+            weight = reader.nextLine();
         }
+
+        return new String[]{height, weight};
     }
+
+
 
     // I don't know if writing a "main" method here is appropriate.
     public static void main(String[] args) {
 
         Scanner reader = new Scanner(System.in);
+        //Instantiate a RunCommand Class here
+        RunCommand commandExecutor = new RunCommand(0);
 
         System.out.println("We will start from some basic information.");
-        String[] BasicUserInfo = getBasicUserInfo(reader);
+        String[] basicUserInfo = getBasicUserInfo(reader);
         System.out.println("Now, we would like to know some of your personal data.");
-        String[] PersonalUserInfo = getPersonalUserInfo(reader);
+        String[] personalUserInfo = getPersonalUserInfo(reader);
 
-        // Now we pass down the two arrays to the Controller class which creates the user objects
-        // TODO: wait for the Controller class to be written so we can implement its method.
+        // Pass in the two arrays to the commandExecutor, and instantiate the classes accordingly.
+        commandExecutor.createUser(basicUserInfo, personalUserInfo);
 
-        System.out.println("Welcome, " + BasicUserInfo[0] + ", What would you like to do today?");
-        System.out.println("You may choose the following options: \n" +
-                "1. Check your BMI \n");
-        int command = Integer.valueOf(reader.nextLine());
 
-        operations(command);
+        System.out.println("Welcome, " + basicUserInfo[0] + ", What would you like to do today?");
+        System.out.println(" You may choose the following options: \n" +
+                           " 1. Analyze Body Mass Index (BMI) \n" +
+                           " 2. Analyze Energy Required per day (EER) \n" +
+                           " 3. Analyze Workout \n" +
+                           " 4. Analyze Disease \n" +
+                           " 5. Generate a meal plan \n");
+        int command = Integer.parseInt(reader.nextLine());
+
+        // Update the commandExecutor's commandNumber variable.
+        commandExecutor.setCommandNumber(command);
+        // Then we do things in the commandExecutor.
+
+
+
 
 
     }
