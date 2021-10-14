362323bcbb6411a519d458eb88b02284753088d0
Winnie Huang <winnie.huang@mail.utoronto.ca>
2021-10-14 00:18:11 -0400

    Changed parameters for runCommand. Created User in runCommand instead of COnsole. Fixed minor mistakes in BMI analyzer equation.

--- src/main/java/BMIAnalyzer.java
+++ src/main/java/BMIAnalyzer.java
@@ -1,5 +1,5 @@
 package main.java;
-
+import static java.lang.Math.round;
 import java.util.HashMap;
 
 public class BMIAnalyzer extends UserAnalyzer {
@@ -12,9 +12,28 @@ public class BMIAnalyzer extends UserAnalyzer {
         HashMap<String, Object> personalData = user.getPersonalData();
         float userHeight = Float.parseFloat((String)personalData.get("height"));
         float userWeight = Float.parseFloat((String)personalData.get("weight"));
-        float bmi = (userHeight / (userWeight * userWeight));
+        float bmi = (userWeight / (userHeight * userHeight));
         user.setPersonalData("BMI", bmi);
-        return bmi;
+
+        String intro =  "The Body Mass Index (BMI) is a number calculated given your weight and height. \n" +
+                "High BMI (>25.0) can indicate high body fatness, and considered as overweight. \n" +
+                "BMI can indicate health problems such as obesity and malnutrition.\nYour Body Mass Index is " +
+                Math.round(bmi*100)/100;
+
+        String health;
+        if (bmi >= 30){
+            health = "Obesity.";
+        }
+        else if (bmi >= 25){
+            health = "OverWeight.";
+        }
+        else if (bmi >= 18.5) {
+            health = "Healthy Weight.";
+        }
+        else{
+            health = "Underweight.";
+        }
+        return intro + ".\nThis is considered: " + health;
     }
 
 }
--- src/main/java/RunCommand.java
+++ src/main/java/RunCommand.java
@@ -12,52 +12,26 @@ public class RunCommand {
     public RunCommand(){}
 
 
-//    /**
-//     * Executes the provided command line accordingly
-//     * @param command is the given command
-//     * @return string that is given back as command is executed
-//     * @throws Exception if the provided command is invalid
-//     */
-//    public String executeCommand(int command) throws Exception {
-//
-//        switch(command) {
-//            case "Use Case 1":
-//                // TODO: call use case function here and return string
-//                break;
-//                return "";
-//            case "Use Case 2":
-//                // TODO: call use case function here and return string
-//                break;
-//                return "";
-//            default:
-//                // TODO: Decide where to catch exception, and also what is the exception text
-//                throw new Exception("Some error text to indicate command is invalid");
-//        }
-//        // What do we return here?
-//    }
+    /**
+     * Executes the provided command line accordingly
+     * @param command is the given command
+     * @return string that is given back as command is executed
+     * @throws Exception if the provided command is invalid
+     */
+    public String executeCommand(int command, String[] basic, String[] personal) throws Exception {
+
+        User newUser = CreateUser.newUser(basic, personal);
+
+        switch(command) {
+            case 1:
+                BMIAnalyzer bmi = new BMIAnalyzer();
+
+               return (String) bmi.analyze(newUser);
+
+            default:
+                throw new Exception("Sorry, the command you have entered is invalid. Please re-enter.");
+        }
+    }
     // I'm commenting this method out for now or else it gives an error in Console.main().
 
-    /**
-     * Generates a new User object based on the information entered by the user in Console.
-     * @param basicUserInfo An array of strings in the format of {name, age, gender}.
-     * @param personalUserInfo An array of strings in the format of {height, weight}.
-     */
-    public void generateUser(String[] basicUserInfo, String[] personalUserInfo){
-        this.tempUser = CreateUser.newUser(basicUserInfo, personalUserInfo);
-    }
-
-    /**
-     * Execute the commands accordingly based on the user's output.
-     * @param command An integer representing what the user wants to do.
-     */
-    public void executeCommand(int command){
-        if (command == 1){
-            BMIAnalyzer analyzer = new BMIAnalyzer();
-            float bmi = (Float) analyzer.analyze(tempUser);
-//            Presenter presenter = new Presenter();
-//            What do I do with the presenter exactly?
-            System.out.println("Your BMI is: " + bmi);
-        }
-
-    }
 }
