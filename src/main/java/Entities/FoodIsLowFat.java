245c2af6881b0a95bccb6b540e74fb65909610f0
DAVIDMA0808 <jianxiang.ma@mail.utoronto.ca>
2021-11-13 10:47:12 -0500

    Created IFood and made relative changes in Food-related classes.

--- src/main/java/Entities/FoodIsLowFat.java
+++ src/main/java/Entities/FoodIsLowFat.java
@@ -6,7 +6,7 @@ package Entities;
  */
 public class FoodIsLowFat extends FoodFilterCriterion{
     @Override
-    public boolean isSatisfiedBy(Food food) {
+    public boolean isSatisfiedBy(IFood food) {
         return food.getFat() < 0.2;     // i.e. food has percentage of fat under 20% of the recommended daily value of intake.
     }
 }
