b0d273e10889364cc2d32d49fb404cffeaca413e
jennylin331431 <jennylin331431@gmail.com>
2021-11-29 21:58:44 -0500

    added additional test cases for DiseaseAnalyzer

--- src/test/java/TestDiseaseAnalyzer.java
+++ src/test/java/TestDiseaseAnalyzer.java
@@ -27,7 +27,7 @@ public class TestDiseaseAnalyzer {
     }
 
     @Test(timeout = 500)
-    public void testOutputFinalDiseases() {
+    public void testGenerateFinalDiseases() {
         user.addRiskFactor("yellowish_skin");
         user.addRiskFactor("nausea");
         disease.analyze();
@@ -51,24 +51,24 @@ public class TestDiseaseAnalyzer {
 
         //because everytime the symptoms are randomly generated, we can only check whether the options generated
         //actually correspond to diseases that include the given symptom.
+        //these are the possible symptoms prompted
         String result = disease.getResult();
+
+        // convert them into list in desired format for program
         result = result.replaceAll("[\\[\\](){}]", "");
         String[] symptomsList = result.split(",");
-        //These are the possible options to be generated
-        // converted into list
         List<String> finalOptionsList;
-
         finalOptionsList = Arrays.asList(symptomsList);
         ArrayList<String> finalOptionsList1 = new ArrayList<>();
-
         for (String item : finalOptionsList){
            item = item.trim();
            finalOptionsList1.add(item);
         }
 
+        //Get all the dataset
         HashMap<String, Set<String>> dataset = DiseaseAPI.readFromDiseaseCSV();
         ArrayList<String> allSymptoms = new ArrayList<>();
-        //All possible symptoms to be generated in the options
+        //Collect all possible symptoms to be generated in the options by filtering using the risk factors of the user
         for (String disease: dataset.keySet()){
             if(dataset.get(disease).containsAll(user.getRiskFactor())){
                 allSymptoms.addAll(dataset.get(disease));
@@ -79,4 +79,49 @@ public class TestDiseaseAnalyzer {
         //check whether the options generated are truly contained in the possible options
         assertTrue(allSymptoms.containsAll(finalOptionsList1));
     }
+    @Test(timeout = 500)
+    public void testNoValidDiseases() {
+        user.addRiskFactor("yellowish_skin");
+        user.addRiskFactor("nausea");
+        user.addRiskFactor("continuous_sneezing");
+        user.addRiskFactor("acidity");
+        disease.analyze();
+        assertEquals("These are your potential diseases: (if output = [], there is no disease that match " +
+                        "the current symptoms you are experiencing)\n" +
+                        "[]",
+                disease.getResult());
+    }
+
+//    @Test(timeout = 500)
+//    public void testNoRiskFactor() {
+//        disease.analyze();
+//        //These are the options generated
+//        String result = disease.getResult();
+//        result = result.replaceAll("[\\[\\](){}]", "");
+//        String[] symptomsList = result.split(",");
+//        // convert them into list in desired format for program
+//        List<String> finalOptionsList;
+//
+//        finalOptionsList = Arrays.asList(symptomsList);
+//        ArrayList<String> finalOptionsList1 = new ArrayList<>();
+//
+//        for (String item : finalOptionsList){
+//            item = item.trim();
+//            finalOptionsList1.add(item);
+//        }
+//        //Get all Diseases from Data set
+//        HashMap<String, Set<String>> dataset = DiseaseAPI.readFromDiseaseCSV();
+//        ArrayList<String> allSymptoms = new ArrayList<>();
+//        //Collect all possible symptoms to be generated in the options
+//        for (String disease: dataset.keySet()){
+//            for(String item: finalOptionsList) {
+//                if (dataset.get(disease).contains(item)) {
+//                    allSymptoms.remove(item);
+//                }
+//            }
+//            }
+//        //Check whether the prompted symptoms belong to the data set
+//            assertTrue(allSymptoms.isEmpty());
+//
+//        }
 }
