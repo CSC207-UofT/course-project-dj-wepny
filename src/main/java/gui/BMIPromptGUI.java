903d41882037e1b433e2f28f472c0dbe813ecf1c
yifanl27 <lihello54321@gmail.com>
2021-12-05 00:21:04 -0500

    Modified BMIPromptGUI so that it now has a header and fonts are changed

--- src/main/java/gui/BMIPromptGUI.java
+++ src/main/java/gui/BMIPromptGUI.java
@@ -1,9 +1,13 @@
 package gui;
 
+import constants.GUIFormatConstants;
 import controllers.Presenter;
 import controllers.RunCommand;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
 
 /**
  * This class is the Page for BMIAnalyzer function.
@@ -15,7 +19,9 @@ public class BMIPromptGUI extends JFrame {
     private JPanel BMIPromptGUI;
     private JTextPane result;
     private JButton returnToMenu;
+    private JLabel headerImgLabel;
     private final RunCommand commandExecutor = new RunCommand(1);
+    private BufferedImage headerImg;
 
     public BMIPromptGUI(){
         super("DJ WEPNY Personal Health Aid");
@@ -55,4 +61,13 @@ public class BMIPromptGUI extends JFrame {
 
     }
 
+    private void createUIComponents() {
+        try{
+            headerImg = ImageIO.read(GUIFormatConstants.bmiGeneratorImgFile);
+        }catch (IOException ex){
+            System.out.println("File pathway was not found");
+        }
+
+        headerImgLabel = new JLabel(new ImageIcon(headerImg));
+    }
 }
